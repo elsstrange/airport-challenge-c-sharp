826547a737bfc4c87790ad0edf42722619490ad5
elsstrange <ellenabstrange@gmail.com>
2021-02-26 15:52:51 +0000

    Plane lands when in flight.

--- ConsoleApp2/Plane.cs
+++ ConsoleApp2/Plane.cs
@@ -2,11 +2,24 @@
 
 public class Plane
 {
+	private bool InFlight
+	{ get; set; }
+
 	public Plane()
 	{
+		InFlight = false;
 	}
 
 	public void TakeOff()
+    {
+		if(InFlight)
+        {
+			throw new InvalidOperationException("Cannot Take Off: Already in flight");
+        }
+		InFlight = true;
+    }
+
+	public void Land()
     {
 
     }
--- NUnitTestProject1/PlaneTest.cs
+++ NUnitTestProject1/PlaneTest.cs
@@ -20,6 +20,26 @@ namespace AirportChallenge.Tests
 				() => { testPlane.TakeOff(); },
 				Throws.Nothing);
         }
+
+		[Test]
+		public void TakeOff_should_throw_error_if_plane_is_in_flight()
+        {
+			string takeOffError = "Cannot Take Off: Already in flight";
+			testPlane.TakeOff();
+			Assert.That(
+				() => { testPlane.TakeOff(); },
+				Throws.InvalidOperationException
+                .With.Property("Message").EqualTo(takeOffError));
+        }
+
+		[Test]
+		public void Land_should_execute_if_plane_is_in_flight()
+        {
+			testPlane.TakeOff();
+			Assert.That(
+				() => { testPlane.Land(); },
+				Throws.Nothing);
+        }
 	}
 
 }
