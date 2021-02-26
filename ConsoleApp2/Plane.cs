a0793dc024d4448975e8e229c877312ee2e491e3
elsstrange <ellenabstrange@gmail.com>
2021-02-26 15:37:25 +0000

    Plane takes off

--- ConsoleApp2/Plane.cs
+++ ConsoleApp2/Plane.cs
@@ -5,4 +5,9 @@ public class Plane
 	public Plane()
 	{
 	}
+
+	public void TakeOff()
+    {
+
+    }
 }
---
+++ NUnitTestProject1/PlaneTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace AirportChallenge.Tests
+{
+	[TestFixture]
+	public class PlaneTest
+	{
+		Plane testPlane;
+
+		[SetUp]
+		public void SetUp()
+        {
+			testPlane = new Plane();
+        }
+
+		[Test]
+		public void TakeOff_should_execute_if_plane_is_landed()
+        {
+			Assert.That(
+				() => { testPlane.TakeOff(); },
+				Throws.Nothing);
+        }
+	}
+
+}
